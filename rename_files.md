0e2caafa892b0ca709e0cecf5e61867484ea79aa
azavgo <azavgo@yahoo.com>
2025-08-28 22:08:21 +1000

    Added random string generation for renaming the files

--- rename_files.md
+++ rename_files.md
@@ -1,5 +1,16 @@
-1. Allow an argument to the program and print that argument, for example: 
+Type 
 ```console
-   ./rename_files my_argument
-   my_argument
-```
+      cargo run -- argument
+``` 
+to pass an argument to the program.  
+
+1. Pass an argument to the program and print that argument, for example: 
+   ```console
+      ./rename_files my_argument
+      my_argument
+   ```
+1. Modify the argument by adding "-" and random alphanumeric eight characters, e.g.:  
+   ```console
+      ./rename_files my_argument
+      my_argument-jy8o04ec
+   ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::{args};
+use rand::prelude::*;  
+
+pub fn random_string(n: u8) -> String {
+    let random_characters_set = 
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+        'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', 
+        '4', '5', '6', '7', '8', '9'];
+
+    let mut rng = rand::rng();
+    let mut rename_set: Vec<char> = vec!['-'];
+
+    for _i in 0..n {
+        rename_set.push(random_characters_set[rng.random_range(0..36)]); 
+    }
+    rename_set.into_iter().collect()
 }
+
+fn main() {
+    match args().nth(1) {
+        Some(file) => {
+            let rename_string = random_string(8); 
+            let file_renamed = format!("{file}{rename_string}"); 
+            println!("{}", file_renamed);}, 
+        None => println!("No argument passed"), 
+    } 
+}
+ 
